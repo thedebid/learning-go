3be06c7c23f9b89db2620cd31b3e2160a916b5d6
Debid Magar <linkwithdm@gmail.com>
2021-12-23 22:11:03 +0545

    Go Basic Variables

---
+++ Basic/index.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+
+func main(){
+	//hello world
+	fmt.Println("Hello World")
+
+	//case sensetive
+	var val = 1
+	var Val = 2
+	fmt.Println(val)
+	fmt.Println(Val)
+
+	//data types
+	fmt.Println(5+5)
+	fmt.Println("5+5")
+	
+	//check data type of variable
+	var str = "go"
+	var val1 = 5.5
+	fmt.Printf("%T\n",str);
+	fmt.Printf("%T\n",val1);
+
+	//varibales
+	var var2 int
+	var2 = 10
+	var var3,var4 = "Hello World",9
+	fmt.Println(var2)
+	fmt.Println(var3)
+	fmt.Println(var4)
+	const x = 90
+	fmt.Println(x)
+
+}
